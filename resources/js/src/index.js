9a960580a0e2dbb7e92fa77e9986dd9ee7cf50b7
CarloCloudEmployee <carlo@cloudemployee.co.uk>
2016-05-16 23:26:06 +0800

    Add the Modal for gallery and floor plan

--- resources/js/src/index.js
+++ resources/js/src/index.js
@@ -12,7 +12,7 @@ import slick from "./../vendor/slick/rdslick.js";
 $(document).ready(init);
 
 function init(){
-    $.fn.slick = slick;
+	$.fn.slick = slick;
 	runImportedFunctions();
 }
 
@@ -23,20 +23,63 @@ function runImportedFunctions(){
 	modal();
 	themeSwitch();
 
+	//$.fn.rdslick = slick; 
+	$('.slider-navigation').slick({  
+		dots: false,
+		speed: 300,
+		slidesToShow: 9,
+		centerMode: true,
+		vertical: true,
+		infinite: true, 
+		arrow: true,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 5,
+		      }
+		    }
+	    ]
+	});
+
+	$('.slider-main').slick({  
+		dots: false,
+		speed: 300,
+		slidesToShow: 1,
+		centerMode: true,
+		fade: true, 
+		arrow: false,
+		responsive: [
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        arrow: true
+		      }
+		    }
+	    ]
+	});
+
+
+	//console.log($('.slick-current').data('slide'));
+
+
+	$('.slider-navigation .slider-item').on('click', (e) => {
+		let slideTarget = $(e.target).closest('.slider-item').data('slide');
+		var slideIndex = $(this).index();
+		console.log(slideIndex + 3);
+		$( '.slider-main' )[0].slick.slickGoTo( parseInt(slideTarget - 1) );
+		//let slideTarget = $(e.target).closest('.slider-item');
+	});
+
 	$(".placeholder + input").keyup(function() {
-        if($(this).val().length) {
-            $(this).prev('.placeholder').hide();
-        } else {
-            $(this).prev('.placeholder').show();
-        }
-    });
-    $(".placeholder").click(function() {
-        $(this).next().focus();
-    });
-
-    console.log($.fn.slick);
-    console.log(slick);
-
-    $("#slidertest").slick();
-	
+		if($(this).val().length) {
+			$(this).prev('.placeholder').hide();
+		} else {
+			$(this).prev('.placeholder').show();
+		}
+	});
+	$(".placeholder").click(function() {
+		$(this).next().focus();
+	});
 }
--- resources/js/src/modal.js
+++ resources/js/src/modal.js
@@ -1,8 +1,14 @@
 import $ from "./../vendor/jquery/dist/jquery";
+import slick from "./../vendor/slick/rdslick.js";
+
+$.fn.slick = slick;
 
 var modal = () => {
 
   var $modal = $("#modal-container"),
+      $gallery = $('.modal-gallery'),
+      $plan = $('.modal-plan'),      
+
 
   init = () => {
 
@@ -11,8 +17,26 @@ var modal = () => {
       $('.backdrop').addClass('isActive');
     });
 
+    $('.btn-gallery-cta').on('click', () => {
+      $gallery.addClass('isActive');
+      $('.backdrop').addClass('isActive');
+    });
+
+    $('.btn-floor-cta').on('click', () => {
+      $plan.addClass('isActive');
+      $('.backdrop').addClass('isActive');
+    });
+
+    $('.btn-modal-close').on('click', () => {
+      $gallery.removeClass('isActive');
+      $plan.removeClass('isActive');
+      $('.backdrop').removeClass('isActive');
+    });
+
     $('.backdrop').on('click', () => {
       $modal.removeClass('isActive');
+      $plan.removeClass('isActive');
+      $gallery.removeClass('isActive');
       $('.backdrop').removeClass('isActive');
     });
   }
